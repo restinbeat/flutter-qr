e5f68ea84beca778177834d80c160e9ae69709ce
jungkiju <thek226@gmail.com>
2021-08-15 23:06:20 +0900

    Upload file to post

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -33,7 +36,14 @@ class _CreatePageState extends State<CreatePage> {
 
   // 갤러리에서 사진 가져오기
   Future _getImage() async {
-
+    var image = await ImagePicker.pickImage(
+        source: ImageSource.gallery,
+        maxWidth: 640,
+        maxHeight: 480
+    );
+    setState(() {
+      _image = image;
+    });
   }
 
   @override
@@ -60,14 +70,33 @@ class _CreatePageState extends State<CreatePage> {
 
   Future _uploadFile(BuildContext context) async {
     // 스토리지에 업로드할 파일 경로
+    final firebaseStorageRef = FirebaseStorage.instance
+        .ref() // 여기까지 firebase root path
+        .child('post') // 하위 폴더 개념
+        .child('${DateTime.now().microsecondsSinceEpoch}.png');
 
     // 파일 업로드
+    final task = firebaseStorageRef.putFile(
+      _image,
+      StorageMetadata(contentType: 'image/png'),
+    );
 
     // 완료까지 기다림
+    final storageTaskSnapshot = await task.onComplete;
 
     // 업로드 완료 후 url
+    final downloadUrl = await storageTaskSnapshot.ref.getDownloadURL();
 
     // 문서 작성
+    await Firestore.instance.collection('post').add(
+        {
+          'contents': textEditingController.text,
+          'displayName': widget.user.displayName,
+          'email': widget.user.email,
+          'photoUrl': downloadUrl,
+          'userPhotoUrl': widget.user.photoUrl,
+        }
+    );
 
     // 완료 후 앞 화면으로 이동
     Navigator.pop(context);
@@ -150,20 +179,18 @@ class _CreatePageState extends State<CreatePage> {
     return _image == null
         ? Text('No Image')
         : Image.file(
-            _image,
-            width: 50,
-            height: 50,
-            fit: BoxFit.cover,
-          );
+      _image,
+      width: 50,
+      height: 50,
+      fit: BoxFit.cover,
+    );
   }
 
   Widget _buildLocation() {
     final locationItems = [
-      '꿈두레 도서관',
-      '경기도 오산',
-      '오산세교',
-      '동탄2신도시',
-      '동탄',
+      '씨티빌딩',
+      '쌍립빌딩',
+      'ENA빌딩',
       '검색',
     ];
     return SizedBox(
